c4de5677d365c45cdf91a43b90b79aa4c9aa1744
Vu Hong Viet <103104318+vietvh-3042@users.noreply.github.com>
2024-02-28 10:14:09 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export default function App(): JSX.Element {
+  return <button className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>...</button>;
+}
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT),
+    },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  };
+});
